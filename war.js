6c4c77c6585f67b1e528421c43a9d5049ae3686b
ChuckManBro <118929288+ChuckManBro@users.noreply.github.com>
2023-01-20 13:21:10 -0700

    Game (war.js) completed

--- war.js
+++ war.js
@@ -1,10 +1,10 @@
 'use strict';
 // WEEK-6 CODING ASSIGNMENT - WAR CARD GAME
 
-// Create a 'Deck' class that creates a full deck array, with a method to shuffle:
+// Create a 'DeckOfCards' class that creates a 'deck' array, with a methods to create a 'newDeck' to 'shuffle':
 // Each card in the deck will have a value, a suit, and a rank (Aces low, with a rank of 1).
 
-class Deck {
+class DeckOfCards {
 	deck = [];
 	newDeck() {
 		const suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
@@ -43,12 +43,12 @@ class Deck {
 	}
 }
 
-let cards = new Deck();
+const cards = new DeckOfCards();
 cards.newDeck();
 cards.shuffle();
-console.table(cards.deck);
+// console.table(cards.deck); //ANCHOR - check full deck of shuffled cards
 
-// Player Class will include the player's hand of cards array, score, and name constructor
+// Player Class will include the player's 'hand' of cards array, 'score', and 'name' constructor
 
 class Player {
 	hand = [];
@@ -58,18 +58,65 @@ class Player {
 	}
 }
 
-// Create two players from the Player class:
+// Create two players from the 'Player' class:
 
-let player1 = new Player('Joe');
-let player2 = new Player('Lucy');
+const player1 = new Player('Ben');
+const player2 = new Player('Lucy');
 
 // Distribute half of the shuffled deck to each player:
 
 player1.hand = player1.hand.concat(cards.deck.splice(0, 26));
 player2.hand = cards.deck;
-console.table(player1.hand);
-console.table(player2.hand);
+// console.table(player1.hand); //ANCHOR - check player1's hand
+// console.table(player2.hand); //ANCHOR - check player2's hand
 
-//loop to play cards, and record score of each round, and add to total scores
+// Loop to play a round, tabulate scores, and declare round winner
+
+function playGame() {
+	for (let round = 1; round <= 26; round++) {
+		console.log(`\n==============================\nROUND ${round}:`);
+		const player1play = player1.hand.shift();
+		const player2play = player2.hand.shift();
+		let indefiniteArticle1 = 'a';
+		if (player1play.value === 8 || player1play.value === 'Ace') {
+			indefiniteArticle1 = 'an';
+		}
+		let indefiniteArticle2 = 'a';
+		if (player2play.value === 8 || player2play.value === 'Ace') {
+			indefiniteArticle2 = 'an';
+		}
+		console.log(
+			`${player1.name} played ${indefiniteArticle1} ${player1play.value} of ${player1play.suit}`
+		);
+		console.log(
+			`${player2.name} played ${indefiniteArticle2} ${player2play.value} of ${player2play.suit}`
+		);
+		if (player1play.rank > player2play.rank) {
+			player1.score++;
+			console.log(`${player1.name}(${player1.score}-${player2.score}) wins this round`);
+		} else if (player2play.rank > player1play.rank) {
+			player2.score++;
+			console.log(`${player2.name}(${player2.score}-${player1.score}) wins this round`);
+		} else {
+			console.log(`This round was a tie.`);
+		}
+	}
+}
+playGame();
 
 // display final scores, and declare the winner (or tie)
+
+function declareWinner() {
+	let declaration = '';
+	if (player1.score > player2.score) {
+		declaration = `🏆 ${player1.name} wins the game, ${player1.score} to  ${player2.score}! 🏆`;
+	} else if (player2.score > player1.score) {
+		declaration = `🏆 ${player2.name} wins the game, ${player2.score} to  ${player1.score}! 🏆`;
+	} else {
+		declaration = `The game ended in a tie!`;
+	}
+	console.log(
+		`\n===================================\n${declaration}\n===================================`
+	);
+}
+declareWinner();
